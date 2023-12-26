6a0db71d07836446b863b529c1b652155602bfe0
naveengitboi <naveenjangiti9@gmail.com>
2023-12-26 10:28:46 +0530

    added dashboard files

---
+++ frontend/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import '../css/Dashboard.css'
+
+function Dashboard(){
+    return(
+        <div className="dashboardContainer">
+            <h2>HAoo</h2>
+        </div>
+    )
+}
+
+
+export default Dashboard;
