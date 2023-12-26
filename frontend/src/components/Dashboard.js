1e393ba225a4a4d9e33887a70704e5bf4a6b5377
naveengitboi <naveenjangiti9@gmail.com>
2023-12-26 11:39:21 +0530

    added profile container

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,9 +1,33 @@
 import '../css/Dashboard.css'
-
+import { IoMdArrowDropright } from "react-icons/io";
+import { NavLink } from 'react-router-dom';
 function Dashboard(){
     return(
         <div className="dashboardContainer">
-            <h2>HAoo</h2>
+            <div className="dashboard">
+                <div className="profileContainer">
+                    <p>Hello</p>
+                    <p className='userName'>naveengitboi</p>
+                    <p>X semester</p>
+                </div>
+                <div className="operationsContainer">
+                    <div className="operation">
+                        <NavLink to='/attendance'>Attendance</NavLink>
+                        <div className="dashboardIcons">
+                            <IoMdArrowDropright/>
+                        </div>
+                    </div>
+
+                    <div className="operation">
+                        <NavLink to='/learnings'>Learnings</NavLink>
+
+                        <div className="dashboardIcons">
+                            <IoMdArrowDropright />
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
         </div>
     )
 }
