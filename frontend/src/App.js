9326867068229bc439a47b84e3c4c60e3f08a2fb
naveengitboi <naveenjangiti9@gmail.com>
2023-12-26 10:17:39 +0530

    added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <>
-      
+    <div className='appjs'>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<h2>Home</h2>} />
         <Route path="/about" element={<h2>About</h2>} />
       </Routes>
-    </>
+    </div>
   );
 }
 
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,31 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import '../css/Navbar.css'
+import { MdDashboard, MdOutlineSettings, MdEditNote, MdNotes, MdVerifiedUser, MdNotificationsActive, MdNote } from "react-icons/md"; 
+import { TiAdjustBrightness, TiWeatherSunny } from "react-icons/ti";
 function Navbar(){
     return(
         <>
             <nav className="navbar">
-                <div className="linksContianer">
+                <div className="navLinksContainerDesktop">
                     <ul className="navLinks">
-                        <NavLink  ></NavLink>
+                        <li><NavLink to='/'> <div className="navIcon"><MdDashboard /></div> Dashboard  </NavLink></li>
+                        <li><NavLink to='/notification'>  <div className="navIcon"><MdNotificationsActive /></div> Notifications  </NavLink></li>
+                        <li><NavLink to='/notes'> <div className="navIcon"><MdEditNote /></div>  Notes  </NavLink></li>
+                        <li><NavLink to='/settings'>  <div className="navIcon"><MdOutlineSettings /></div> Settings  </NavLink></li>
+                        <li><NavLink to='/mode'>  <div className="navIcon"><TiAdjustBrightness /></div>  Mode  </NavLink></li>
                     </ul>
+                    <div className="navProfileDiv">
+                        <NavLink to='/profile'>
+                            <div className="profileImage"></div>
+                            Profile
+                        </NavLink>
+                    </div>
                 </div>
             </nav>
         </>
     )
 }
+
+
+export default Navbar
